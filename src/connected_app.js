467861246afc7d3f1d7f59bc2ece992e0ef84c5c
Kevan <kevan.moothien@gmail.com>
2024-05-26 21:55:55 +0400

    change path again

--- src/connected_app.js
+++ src/connected_app.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function runPuppeteer(url) {
-  const browser = await puppeteer.launch({ headless: true, executablePath: '/usr/bin/chromium-browser' });
+  const browser = await puppeteer.launch({ headless: true, executablePath: '/root/.cache/puppeteer' });
   const page = await browser.newPage();
 
   // Set user agent
