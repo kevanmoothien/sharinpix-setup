59a02ce5f5638eae8b2461daa138b8386fc2bd76
Kevan <kevan.moothien@gmail.com>
2024-05-26 21:52:24 +0400

    remove phantomjs

--- .coolify/start.sh
+++ .coolify/start.sh
@@ -3,17 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y chromium-browser
 
-if [ ! -f bin/phantomjs ]; then
-  echo "PhantomJS not found, installing..."
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-  tar -xf phantomjs-2.1.1-linux-x86_64.tar.bz2
-  mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs bin/
-  rm -rf phantomjs-2.1.1-linux-x86_64*
-else
-  echo "PhantomJS already installed."
-fi
-
 yarn install
-yarn start
 
 
